3b6b82bfb7f4da27d67739f54242f844ef1e1de3
paperworkmonkey <161349050+paperworkmonkey@users.noreply.github.com>
2025-09-11 14:53:49 +0100

    Add files via upload
    Search term - multiple terms and completion mapping added

--- ICNARCcodeFinder.js
+++ ICNARCcodeFinder.js
@@ -1,6 +1,12 @@
 let data;
 let searchTerm;
-let searchTermBox, myResultsBox, myICNARCsystem, myICNARCsite, myICNARCprocess, myICNARCcondition, myICNARCcode;
+let searchTermBox,
+  myResultsBox,
+  myICNARCsystem,
+  myICNARCsite,
+  myICNARCprocess,
+  myICNARCcondition,
+  myICNARCcode;
 
 function preload() {
   data = loadTable("ICNARCcodetable3.csv", "csv", "header");
@@ -8,30 +14,41 @@ function preload() {
 
 function getSearchTerm() {
   searchTerm = inputValue.value.toLowerCase();
-  console.log(searchTerm);
-  let myRows = data.matchRows(searchTerm, "lowerCaseCondition");
+  let mySearchTermArray = searchTerm.split(" ");
+  console.log(mySearchTermArray);
+
+  let myInitialRows = data.matchRows(
+    mySearchTermArray[0],
+    "lowerCaseCondition"
+  );
+  console.log(myInitialRows);
+
+  let myRows = myInitialRows.filter((row) =>
+    mySearchTermArray.every((term) =>
+      row.arr.some((val) => val.toLowerCase().includes(term.toLowerCase()))
+    )
+  );
+
   let SystemString = "";
   let SiteString = "";
   let ProcessString = "";
   let ConditionString = "";
   let CodeString = "";
 
-  
-  console.log(myRows);
+  //console.log(myRows);
   for (let i = 0; i < myRows.length; i++) {
-    SystemString += myRows[i].getString('System') + "<br>";
-    SiteString += myRows[i].getString('Site') + "<br>";
-    ProcessString += myRows[i].getString('Process') + "<br>";
+    SystemString += myRows[i].getString("System") + "<br>";
+    SiteString += myRows[i].getString("Site") + "<br>";
+    ProcessString += myRows[i].getString("Process") + "<br>";
     ConditionString += myRows[i].getString("Condition") + "<br>";
     CodeString += myRows[i].getString("ICM V4.0") + "<br>";
   }
-  
+
   myICNARCsystem.html(SystemString);
   myICNARCsite.html(SiteString);
   myICNARCprocess.html(ProcessString);
   myICNARCcondition.html(ConditionString);
   myICNARCcode.html(CodeString);
-
 }
 
 function setup() {
@@ -44,6 +61,7 @@ function setup() {
     data.set(i, "lowerCaseCondition", data.get(i, "Condition").toLowerCase());
   }
 
+  //get DOM elements
   searchTermBox = select("#inputValue");
   myResultsBox = select("#resultsBox");
   myICNARCsystem = select("#ICNARCsystem");
@@ -52,11 +70,10 @@ function setup() {
   myICNARCcondition = select("#ICNARCcondition");
   myICNARCcode = select("#ICNARCcode");
 
-  print(data.getRowCount() + " total rows in table");
-  print(data.getColumnCount() + " total columns in table");
+  // print(data.getRowCount() + " total rows in table");
+  // print(data.getColumnCount() + " total columns in table");
 }
 
 function draw() {
   //background(220);
-
 }
